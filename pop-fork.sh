246048deec9186c1ae9f324b2e69714a1288d184
Seamus Bradley <scmb@system76-pc.localdomain>
2021-04-12 18:56:09 +0100

    Added screenshot

--- pop-fork.sh
+++ pop-fork.sh
@@ -6,4 +6,4 @@ cp $FROM_DIR/README.md $TO_DIR/README.md
 cp $FROM_DIR/taskw.py $TO_DIR/taskw
 cp $FROM_DIR/LICENSE $TO_DIR/LICENSE
 cp $FROM_DIR/i3blocks.conf $TO_DIR/i3blocks.conf
-
+cp $FROM_DIR/taskw-screenshot01.png $TO_DIR/taskw-screenshot01.png
