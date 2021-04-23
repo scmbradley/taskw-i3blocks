765b788f8fd8bc42ea17ef8fb4370133c101496b
Seamus Bradley <github@scmb.xyz>
2021-04-23 17:17:42 +0100

    Added timew override.
    Also added several new boolean config options.

--- README.md
+++ README.md
@@ -6,6 +6,41 @@ and an indication of how many other active tasks you have.
 
 ## Config options
 
+ - `TASKW_TF` : whether to display information about tasks (from taskwarrior)
+ - `TIMEW_TF` : whether to display information about time durations (from timewarrior)
+ - `TIMEW_DESC_OVERRIDE` : whether to pull task description information from taskwarrior (false) or timewarrior (true). Will also set `TIMEW_TF` to true.
  - `TASKW_MAX_LENGTH` : the number of characters to truncate long task descriptions at
  - `TASKW_NOTASK_MSG` : the text to display if there are no active tasks
  - `TASKW_SORT_URGENCY` : a boolean to determine whether to display the most urgent active task (or the default behaviour which is to display the task which has been active longest).
+ 
+
+## Timewarrior integration
+
+Integration between taskwarrior and timewarrior is via a taskwarrior hook that starts/stops timewarrior when you 
+start/stop a task in taskwarrior.
+What this means is that if you have multiple tasks ongoing, the current timew duration is for 
+the *most recently started* task (and the time for the task started earlier will be stopped when a newer task is started).
+So having multiple ongoing tasks doesn't really play nice with timewarrior.
+To make sure the task description matches the task to which the timew duration refers,
+we could do one of two things: 
+optionally sort tasks by most recent start time, or
+get the task description from timew directly.
+The former would require a lot messing about parsing times and dates, 
+so we can explore the latter.
+Timewarrior tags don't have any sort of hierarchy, so there's no analogue of a task duration's "description" attribute in taskwarrior.
+The description is passed to timewarrior as the first tag by the `on-modify` hook.
+
+All this is to explain why only the following modes of use are properly 
+supported.
+
+### Single task taskw/timew integration
+
+Set `TASKW_TF` true and `TIMEW_TF` true.
+Use the standard `on-modify` hook, only ever have *one* ongoing task.
+Description information is reliably drawn from taskwarrior.
+
+### Timew standalone usage
+
+Set `TASKW_TF` false and `TIMEW_TF` true.
+When using `timew`, make sure that the *first* tag is the one you want to appear 
+in the bar.
--- taskw.py
+++ taskw.py
@@ -28,6 +28,22 @@ notask_msg = _default("TASKW_NOTASK_MSG", default="No Task", arg_type=str)
 urgency_bool = _default("TASKW_SORT_URGENCY", default="", arg_type=strbool)
 
 
+taskw_tf = _default("TASKW_TF", default="t", arg_type=strbool)
+timew_tf = _default("TIMEW_TF", default="f", arg_type=strbool)
+timew_desc_override = _default("TIMEW_DESC_OVERRIDE", default="f", arg_type=strbool)
+
+# Set timew_tf to true if the override is set.
+if timew_desc_override:
+    timew_tf = True
+
+##############################
+# TESTING TESTING TESTING
+
+# taskw_tf = False
+# timew_desc_override = True
+##############################
+
+
 def shorten(string):
     if len(string) <= maxlen:
         return string
@@ -35,22 +51,52 @@ def shorten(string):
         return string[: maxlen - 3] + "..."
 
 
-def main():
+def export_taskw():
     shell_cmd = "task +ACTIVE export"
-    prcs = subprocess.check_output(shell_cmd, shell=True)
-    j = json.loads(prcs)
+    j = json.loads(subprocess.check_output(shell_cmd, shell=True))
     max_urg = 0
-    if urgency_bool:
-        for i in range(len(j)):
-            if j[i]["urgency"] > j[max_urg]["urgency"]:
-                max_urg = i
-
-    if len(j) == 0:
-        bar_text = notask_msg
-    elif len(j) == 1:
-        bar_text = shorten(j[max_urg]["description"])
+    if len(j) > 0:
+        if urgency_bool:
+            for i in range(len(j)):
+                if j[i]["urgency"] > j[max_urg]["urgency"]:
+                    max_urg = i
+        return j[max_urg]["description"], len(j)
     else:
-        bar_text = shorten(j[max_urg]["description"]) + " + " + str(len(j) - 1)
+        return notask_msg, 0
+
+
+def export_timew_text():
+    timew_shell = "timew get dom.active.tag.1"
+    # the output of `timew get` includes a newline that we have to strip.
+    timew_txt = subprocess.check_output(timew_shell, shell=True).decode("utf-8")[:-1]
+    return timew_txt
+
+
+def export_timew_active():
+    timew_active_shell = "timew get dom.active"
+    timew_active_tf = subprocess.check_output(timew_active_shell, shell=True)
+    return True if b"1" in timew_active_tf else False
+
+
+def main():
+    task_desc = ""
+    task_append = ""
+    if taskw_tf:
+        descr, task_num = export_taskw()
+        task_desc = shorten(descr)
+        if task_num > 1:
+            task_append = " + " + str(task_num - 1)
+
+    if timew_tf:
+        # pull duration
+        pass
+
+    if timew_desc_override or not taskw_tf:
+        task_desc = (
+            shorten(export_timew_text()) if export_timew_active() else notask_msg
+        )
+    # We will be appending more things to this, later.
+    bar_text = task_desc + task_append
     return bar_text
 
 
